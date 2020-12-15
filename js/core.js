a911d514b459f168e4976b4054d9e74e179e73ea
Joseph McGeever <mcgeevej@coventry.ac.uk>
2020-12-15 12:40:50 +0000

    installed http-server into package.json. changed the api url to the production url

--- js/core.js
+++ js/core.js
@@ -4,6 +4,16 @@
 //export const apiURL = 'https://short-panda-8080.codio-box.uk'
 export const apiURL = 'https://calm-castle-89033.herokuapp.com'
 
+
+export function getApiURL(){
+    if( process.env.PORT){
+        return 'https://calm-castle-89033.herokuapp.com'
+    } else {
+        return'https://short-panda-8080.codio-box.uk'
+    }
+}
+
+
 export function generateToken(user, pass) {
 	const token = `${user}:${pass}`
 	const hash = btoa(token)
